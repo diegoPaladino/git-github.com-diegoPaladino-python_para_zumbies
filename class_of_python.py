bc38d83c02500431fae30a43536420995e501c8d
diegoPaladino <diegopaladinofoto@gmail.com>
2020-08-30 15:54:37 -0300

    202008301554

--- class_of_python.py
+++ class_of_python.py
@@ -1,2 +1,15 @@
 #class_of_python
+#class_name: TWP030 Declarando nossas primeiras variáveis
+#link_class: https://www.youtube.com/watch?v=qHx8SRYqW2E&list=PLUukMN0DTKCtbzhbYe2jdF4cr8MOWClXc&index=4
 
+#some basic calculus to learn some thins about python lenguage
+
+# print(type('abacate'))
+# print(dir('abacate'))
+# help('abacate'.upper)
+# print(help(print))
+a = 42
+b = 'abacate'
+print(a*3)
+
+a = 'banana'
---
+++ resposta.py
@@ -0,0 +1,33 @@
+#resposta
+#edition_mode(5:48 - https://www.youtube.com/watch?v=qHx8SRYqW2E&list=PLUukMN0DTKCtbzhbYe2jdF4cr8MOWClXc&index=4)
+
+resposta = 42
+# print('Resposta para tudo é: ',resposta)
+# print(f'tudo tem {resposta} como sulução')
+# preço = 9.666666
+# print(f'preço é R$ {preço:.2f}')
+
+#operadores relacionais (relational operators)
+
+a = 42
+b = 13
+# print(a > b)
+# print(type(True))
+
+#testes lógicos(logic tests)
+#equal
+a1 = a == 42
+b1 = b == 42
+print('the aswer to "a1" is: ',a1)
+print('the aswer to "b1" is: ',b1)
+
+#different
+a2 = a != 42
+b2 = b != 42
+print('the aswer to "a2" is: ',a2)
+print('the aswer to "b2" is: ',b2)
+
+#or
+
+c1 = a == 42 or b == 42
+print('the aswer to "c1" is: ',c1)
