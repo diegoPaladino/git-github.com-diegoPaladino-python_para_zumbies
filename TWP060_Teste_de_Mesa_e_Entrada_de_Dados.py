59e25411261a37c8a8946fa94d1e369e71cd5f2f
diegoPaladino <diegopaladinofoto@gmail.com>
2020-08-31 15:19:09 -0300

    202008311519

--- TWP060_Teste_de_Mesa_e_Entrada_de_Dados.py
+++ TWP060_Teste_de_Mesa_e_Entrada_de_Dados.py
@@ -2,13 +2,36 @@
 #link do tutorial: https://www.youtube.com/watch?v=mZt2oBlk7OY&t=3s
 #site para análise passo a passo da execução do código, passado no video: http://pythontutor.com
 
-divida = 0
-compra = 10.59
-divida = divida + compra
-print('primeira dívida: R$',divida)
-compra = 50.34
-divida = divida + compra
-print('segunda dívida: R$',divida)
-compra = 4.50
-divida = divida + compra
-print('terceira dívida: R$',divida)
+# divida = 0
+# compra = 10.59
+# divida = divida + compra
+# print('primeira dívida: R$',divida)
+# compra = 50.34
+# divida = divida + compra
+# print('segunda dívida: R$',divida)
+# compra = 4.50
+# divida = divida + compra
+# print('terceira dívida: R$',divida)
+
+#testado no pythontutor, perfeita execução passo a passo. Super ferramenta!
+
+#exemplo de conversão necessária da entrada (que virá como string) de números: (exemple of necessary conversion of entries (that will come as string type) of numbers)
+
+# n1 = input('N1: ')
+# n2 = input('N2: ')
+# print(n1 + n2)
+
+#In the exemple above, the entries will came as string, resulting in the aswer: " 1213 " (assuming we put N1: 12 and N2:13, respectivaly)
+#So, to solution, we declare like int or float:
+# n1 = int(input('N1: '))
+# n2 = int(input('N2: '))
+# print(n1 + n2)    
+
+#in float exemple:
+n1 = float(input('N1: '))
+n2 = float(input('N2: '))
+print(n1 + n2)
+
+#lista de exercicios:
+#bit.ly/PPZPythonExercicios
+#tinyurl.com/PPZPythonExercicios
